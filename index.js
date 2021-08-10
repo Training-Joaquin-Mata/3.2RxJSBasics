f74956b2d1dccaeee34382e22a40f97e762c9076
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-10 14:36:06 -0500

    Creation Operators

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
-import { of, Observable, fromEvent} from 'rxjs';
+import { of, from, interval, timer, range ,Observable, fromEvent, asyncScheduler, asapScheduler} from 'rxjs';
 import { map } from 'rxjs/operators'
 
+//#region Create Observables from dom events using from event
 
-const observer ={
-    // next: value=> console.log(value),
-    // error: error => console.log(error),
-    // complete: () => console.log('Complete!')
-}
+// const observer = {
+//     next: vale => console.log(vale),
+//     error: err => console.log(err),
+//     complete: () => console.log('Complete!')
+// }
 
-const observable = new Observable(subscriber =>{
-let count = 0;
-const id = setInterval(()=>{
-    subscriber.next(count);
-    //subscriber.complete();
-    count +=1;
-}, 1000);
+// const source$ = fromEvent(document, 'click');
 
-return () => {
-    console.log("called")
-    clearInterval(id)
-}
-});
+// const subOne = source$.subscribe(observer);
 
-console.log(`
-start Script
-
-`);
-
-const subscription = observable.subscribe(
-    // observer
-    value=> console.log("next:"+value),
-    error => console.log(error),
-    () => console.log('Complete!')
-); 
-
-const subscription2 = observable.subscribe(
-    // observer
-    value=> console.log("next2:"+value),
-    error => console.log(error),
-    () => console.log('Complete!')
-);
-
-setTimeout(()=>{
-    subscription.unsubscribe(
-    value => console.log(value),
-);
-}, 3500)
-
-setTimeout(()=>{
-    subscription2.unsubscribe(
-    value => console.log(value),
-);
-}, 5000)
+// const subTwo = source$.subscribe(observer);
 
 
-console.log(`
-end Script
-`);
+// setTimeout(()=>{
+//     console.log("Unsubscribing...");
+//     subOne.unsubscribe();
+// }, 4000);
+
+//#endregion
+
+//#region Create observables from static values using of
+
+// const observer = {
+//     next: val => console.log("next: "+ val),
+//     error: err => console.log(err),
+//     complete: () => console.log('Complete!')
+// }
+
+// const source$ = of([1,2],3,4,5)  
+// const subOne = source$.subscribe(observer);
+
+
+// const source2$ = range(1, 5);
+// source2$.subscribe(observer);
+
+//#endregion
+
+//#region Turn arrays, iterators, and promises into observables using from
+
+// const observer = {
+//     next: val => console.log('next', val),
+//     error: err => console.log(err),
+//     complete: () => console.log('Complete!')
+// }
+
+// const values = [1,2,3,4,5]
+// const source$ = from(fetch(
+//     'https://api.github.com/users/octocat'
+// ));
+// source$.subscribe(observer);
+//#endregion
+
+//#region Emit items based on a duration with interval and timer
+
+// // const timer$ = interval(1000);
+
+// //timer$.subscribe(console.log)
+
+// const timer$ = timer(2000, 1000);
+
+// timer$.subscribe(console.log)
+
+//#endregion
