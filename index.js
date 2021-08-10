92733a8f67be2923234f2ae09b27855f78851085
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-10 13:32:02 -0500

    Getting started

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
-import { of } from 'rxjs';
+import { of, Observable, fromEvent} from 'rxjs';
+import { map } from 'rxjs/operators'
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJS').subscribe(console.log);
+
+const observer ={
+    // next: value=> console.log(value),
+    // error: error => console.log(error),
+    // complete: () => console.log('Complete!')
+}
+
+const observable = new Observable(subscriber =>{
+let count = 0;
+const id = setInterval(()=>{
+    subscriber.next(count);
+    //subscriber.complete();
+    count +=1;
+}, 1000);
+
+return () => {
+    console.log("called")
+    clearInterval(id)
+}
+});
+
+console.log(`
+start Script
+
+`);
+
+const subscription = observable.subscribe(
+    // observer
+    value=> console.log("next:"+value),
+    error => console.log(error),
+    () => console.log('Complete!')
+); 
+
+const subscription2 = observable.subscribe(
+    // observer
+    value=> console.log("next2:"+value),
+    error => console.log(error),
+    () => console.log('Complete!')
+);
+
+setTimeout(()=>{
+    subscription.unsubscribe(
+    value => console.log(value),
+);
+}, 3500)
+
+setTimeout(()=>{
+    subscription2.unsubscribe(
+    value => console.log(value),
+);
+}, 5000)
+
+
+console.log(`
+end Script
+`);
